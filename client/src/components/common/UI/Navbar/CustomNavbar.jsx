d82fdeeb9e13fe8942056202baca604ea9ebe936
Ramis <ramismubick@gmail.com>
2023-05-04 17:30:45 +0300

    The current section of the website is highlighted on the navbar

--- client/src/components/common/UI/Navbar/CustomNavbar.jsx
+++ client/src/components/common/UI/Navbar/CustomNavbar.jsx
@@ -1,14 +1,16 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Container, Navbar, Nav } from 'react-bootstrap'
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import cl from './CustomNavbar.module.css';
 import { Context } from "../../../..";
 import LeaveConfirmationModal from "../LeaveConfirmationModal";
 
 const CustomNavbar = () => {
+    const [activeNavLinks, setActiveNavLinks] = useState({ main: '', theory: '', tests: '', practice: '', me: '' });
+    const [showModal, setShowModal] = useState(false);
     const { store } = useContext(Context);
     const router = useNavigate();
-    const [showModal, setShowModal] = useState(false);
+    const location = useLocation();
 
     const logout = async () => {
         try {
@@ -26,19 +28,38 @@ const CustomNavbar = () => {
         }
     };
 
+    useEffect(() => {
+        const states = { main: '', theory: '', tests: '', practice: '', me: '' };
+        if (location.pathname.includes('/main')) {
+            setActiveNavLinks({ ...states, main: 'active-navlink' });
+        }
+        else if (location.pathname.includes('/theory')) {
+            setActiveNavLinks({ ...states, theory: 'active-navlink' });
+        }
+        else if (location.pathname.includes('/tests')) {
+            setActiveNavLinks({ ...states, tests: 'active-navlink' });
+        }
+        else if (location.pathname.includes('/practice')) {
+            setActiveNavLinks({ ...states, practice: 'active-navlink' });
+        }
+        else if (location.pathname.includes('/me')) {
+            setActiveNavLinks({ ...states, me: 'active-navlink' });
+        }
+    }, [location]);
+
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand as={Link} to={store.userRole !== 'MODERATOR' ? "/main" : "/theory"} replace className={`main-font-bold text-white ${cl.navbarItem}`}>GraphXplorers</Navbar.Brand>
+                <Navbar.Brand as={Link} to={store.userRole !== 'MODERATOR' ? "/main" : "/theory"} replace className={`main-font-bold text-white ${cl.navbarItem} ${activeNavLinks.main}`}>GraphXplorers</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className='ms-auto'>
-                        <Nav.Link as={Link} to="/theory" className={`main-font-bold text-white me-2 ${cl.navbarItem}`}>Теория</Nav.Link>
-                        <Nav.Link as={Link} to="/tests" className={`main-font-bold text-white me-2 ${cl.navbarItem}`}>Тесты</Nav.Link>
-                        <Nav.Link as={Link} to="/practice" className={`main-font-bold text-white me-2 ${cl.navbarItem}`}>Решение задач</Nav.Link>
+                        <Nav.Link as={Link} to="/theory" className={`main-font-bold text-white me-2 ${cl.navbarItem} ${activeNavLinks.theory}`}>Теория</Nav.Link>
+                        <Nav.Link as={Link} to="/tests" className={`main-font-bold text-white me-2 ${cl.navbarItem} ${activeNavLinks.tests}`}>Тесты</Nav.Link>
+                        <Nav.Link as={Link} to="/practice" className={`main-font-bold text-white me-2 ${cl.navbarItem} ${activeNavLinks.practice}`}>Решение задач</Nav.Link>
                         {store.userRole === 'MODERATOR' && <Nav.Link as={Link} className={`main-font-bold text-white ${cl.navbarItem}`}
                             onClick={() => setShowModal(true)}><i className='bi bi-box-arrow-right' /> Выйти</Nav.Link>}
-                        {store.userRole === 'USER' && <Nav.Link as={Link} to="/me" className={`main-font-bold text-white ${cl.navbarItem}`}>Личный кабинет <i className='bi bi-person-circle' /></Nav.Link>}
+                        {store.userRole === 'USER' && <Nav.Link as={Link} to="/me" className={`main-font-bold text-white ${cl.navbarItem} ${activeNavLinks.me}`}>Личный кабинет <i className='bi bi-person-circle' /></Nav.Link>}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
